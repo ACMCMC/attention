8dab8acf80c94cdbe7c01dc24539ab829a837a34
ACMCMC <>
2024-07-02 14:43:32 +0100

    Add datasets

--- attention/__main__.py
+++ attention/__main__.py
@@ -8,6 +8,7 @@ from .dataset_eval import eval_ud
 MODEL = "t5-base"
 MODEL = "bert-base-uncased"
 MODEL = "bigscience/bloom-560m"
+MODEL = "HiTZ/latxa-7b-v1"
 
 model = transformers.AutoModelForCausalLM.from_pretrained(MODEL)
 
@@ -22,6 +23,12 @@ path_to_conll_dataset = os.path.join(
 )
 path_to_conll_dataset = os.path.join(
     os.path.dirname(__file__), "../UD_French-Sequoia/fr_sequoia-ud-test.conllu"
+)
+path_to_conll_dataset = os.path.join(
+    os.path.dirname(__file__), "../UD_Turkish-Penn-master/tr_penn-ud-test.conllu"
+)
+path_to_conll_dataset = os.path.join(
+    os.path.dirname(__file__), "../UD_Basque-BDT-master/eu_bdt-ud-test.conllu"
 )
 
 eval_ud(model=model, path_to_conll_dataset=path_to_conll_dataset)
